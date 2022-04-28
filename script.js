b295c2804c5f9bfa4406df142be00a1253c87a86
Raiza Rocha <raizarocha8@gmail.com>
2022-04-28 15:35:05 -0300

    Requisitos 6, 7, e 8

--- script.js
+++ script.js
@@ -12,3 +12,31 @@
 //         console.log(pixel);
 //     }
 // }
+// const pixel = document.querySelectorClass('pixel');
+// pixel.style.color = 'green';
+const firstColor = document.getElementById('black');
+const secondColor = document.getElementById('red');
+const thirdColor = document.getElementById('blue');
+const fourthColor = document.getElementById('green');
+const pixelGrade = document.getElementsByClassName('pixel');
+
+function addRemClasse(event) {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+firstColor.addEventListener('click', addRemClasse);
+secondColor.addEventListener('click', addRemClasse);
+thirdColor.addEventListener('click', addRemClasse);
+fourthColor.addEventListener('click', addRemClasse);
+
+function adicionaClasse(event) {
+  const selected = document.querySelector('.selected');
+  const changeColor = window.getComputedStyle(selected).backgroundColor;
+  event.target.style.backgroundColor = changeColor;
+}
+
+for (const pixel of pixelGrade) {
+  pixel.addEventListener('click', adicionaClasse);
+}
