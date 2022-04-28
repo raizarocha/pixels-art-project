91e10c9dae835a07d20b03f930685fa45a0ca3e2
Raiza Rocha <raizarocha8@gmail.com>
2022-04-28 12:29:42 -0300

    Requisitos 4 e 5

---
+++ script.js
@@ -0,0 +1,14 @@
+// let pixel = document.getElementsByClassName('pixel');
+// console.log(pixel);
+
+// let linha = 5;
+// let coluna = 5;
+
+// for (let index = 0; index < linha; index += 1) {
+//     pixel +=1;
+//     console.log(pixel)
+//     for (let index = 0; index < coluna; index += 1) {
+//         pixel +=1;
+//         console.log(pixel);
+//     }
+// }
